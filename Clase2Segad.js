1a2c578bef93a1938a776f88f3b5d28d40b83724
CaruSegad <carlasegad@gmail.com>
2021-03-26 02:16:30 -0300

    Entregable 2

---
+++ Clase2Segad.js
@@ -0,0 +1,40 @@
+//EJERCICIO 1
+
+/*let inputUserName = prompt("ingrese su edad")
+
+let carla = parseInt(inputUserName)
+
+if (carla > 1000) { 
+alert("El número es mayor a 1000")
+}*/
+
+//EJERCICIO 2
+
+/*let inputUserName = prompt("Buenas")
+
+if (inputUserName === "Hola") {
+console.log("¿ke ace?")
+}*/
+
+//EJERCICIO 3
+
+let inputUserName = prompt("Ingresá tu edad para entrar a Slytherin.")
+
+let edad = parseInt(inputUserName)
+
+if (edad > 10 && edad < 50) {
+alert("Podés ser miembro.")
+}
+
+else if (edad < 10) {
+alert("Nope, sos chico.")
+}
+
+/*else if (edad > 50) {
+alert("Alto viejardo.")
+}*/
+
+else {
+alert("No podés, adiós.")
+} 
+
